a6baefadca95711e4eb7ad407ccbebb609abf216
dontsellshort <michaelhly@gmail.com>
2018-12-26 00:17:57 -0600

    silenced console warning

--- src/components/cdp.jsx
+++ src/components/cdp.jsx
@@ -3,7 +3,7 @@ import React, { Component } from "react";
 class Cdp extends Component {
   render() {
     return (
-      <div class="shadow p-3 mb-5 bg-white rounded">
+      <div className="shadow p-3 mb-5 bg-white rounded">
         <div className="card-body">
           <h5 className="card-title">CDP {this.props.auction.cdp}</h5>
           <p className="card-text">
--- src/components/navbar.jsx
+++ src/components/navbar.jsx
@@ -3,37 +3,40 @@ import React, { Component } from "react";
 class Navbar extends Component {
   render() {
     return (
-      <nav class="navbar navbar-light bg-light shadow-sm p-3 mb-4">
-        <a class="navbar-brand" href="#">
+      <nav className="navbar navbar-light bg-light shadow-sm p-3 mb-4">
+        <a className="navbar-brand" href="#">
           CDP.Auction
         </a>
 
-        <ul class="navbar-nav navbar-expand mr-auto">
-          <li class="nav-item m-2 active">
-            <a class="nav-link" href="#">
-              Home <span class="sr-only">(current)</span>
+        <ul className="navbar-nav navbar-expand mr-auto">
+          <li className="nav-item m-2 active">
+            <a className="nav-link" href="#">
+              Home <span className="sr-only">(current)</span>
             </a>
           </li>
-          <li class="nav-item m-2">
-            <a class="nav-link" href="#">
+          <li className="nav-item m-2">
+            <a className="nav-link" href="#">
               My Auctions
             </a>
           </li>
-          <li class="nav-item m-2">
-            <a class="nav-link" href="#">
+          <li className="nav-item m-2">
+            <a className="nav-link" href="#">
               My Bids
             </a>
           </li>
         </ul>
 
-        <form class="form-inline my-2 my-lg-0">
+        <form className="form-inline my-2 my-lg-0">
           <input
-            class="form-control mr-sm-2"
+            className="form-control mr-sm-2"
             type="search"
             placeholder="AuctionID"
             aria-label="Search"
           />
-          <button class="btn btn-outline-success my-2 my-sm-0" type="submit">
+          <button
+            className="btn btn-outline-success my-2 my-sm-0"
+            type="submit"
+          >
             Search
           </button>
         </form>
