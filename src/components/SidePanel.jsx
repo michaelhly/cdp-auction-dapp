a12cd488516ac8c8033aa30af054b17a43ef2d92
dontsellshort <michaelhly@gmail.com>
2018-12-30 15:01:54 -0600

    added min height to side panel

--- src/components/SidePanel.jsx
+++ src/components/SidePanel.jsx
@@ -4,6 +4,7 @@ import styled from "styled-components";
 const Panel = styled.div`
   position: relative;
   text-align: center;
+  min-height: 450px;
   height: 60vh;
   width: 15vw;
   padding: 2em;
@@ -12,7 +13,6 @@ const Panel = styled.div`
   box-shadow: 0 0.5rem 1rem rgba(0, 0, 0, 0.15);
   background: white;
   min-width: 200px;
-  z-index: -1;
 `;
 
 const SidePanel = props => {
