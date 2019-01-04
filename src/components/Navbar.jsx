d8daa2801c9cf96b8e9bb37ceb97445fc801e533
dontsellshort <michaelhly@gmail.com>
2019-01-04 01:52:15 -0600

    cleaned up navbar and added search handling

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,49 +1,57 @@
-import React, { Component } from "react";
-import { NavLink } from "react-router-dom";
+import React, { useState } from "react";
+import { NavLink, Link } from "react-router-dom";
 
-class Navbar extends Component {
-  render() {
-    return (
-      <nav className="navbar navbar-light bg-light shadow-sm p-3 mb-4">
-        <div className="navbar-brand" to="/">
-          CDP.Auction
-        </div>
+const Navbar = () => {
+  const [search, setSearch] = useState("");
 
-        <ul className="navbar-nav navbar-expand mr-auto">
-          <li className="nav-item m-2 active">
-            <NavLink className="nav-link" to="/">
-              Home <span className="sr-only">(current)</span>
-            </NavLink>
-          </li>
-          <li className="nav-item m-2">
-            <NavLink className="nav-link" to="/myauctions">
-              My Auctions
-            </NavLink>
-          </li>
-          <li className="nav-item m-2">
-            <NavLink className="nav-link" to="/mybids">
-              My Bids
-            </NavLink>
-          </li>
-        </ul>
+  const handleInput = e => {
+    setSearch(e.currentTarget.value);
+  };
 
-        <form className="form-inline my-2 my-lg-0">
-          <input
-            className="form-control mr-sm-2"
-            type="search"
-            placeholder="AuctionID"
-            aria-label="Search"
-          />
+  return (
+    <nav className="navbar navbar-light bg-light shadow-sm p-3 mb-4">
+      <div className="navbar-brand" to="/">
+        CDP.Auction
+      </div>
+
+      <ul className="navbar-nav navbar-expand mr-auto">
+        <li className="nav-item m-2">
+          <NavLink exact className="nav-link" to="/">
+            Home
+          </NavLink>
+        </li>
+        <li className="nav-item m-2">
+          <NavLink className="nav-link" to="/myauctions">
+            My Auctions
+          </NavLink>
+        </li>
+        <li className="nav-item m-2">
+          <NavLink className="nav-link" to="/mybids">
+            My Bids
+          </NavLink>
+        </li>
+      </ul>
+
+      <form className="form-inline my-2 my-lg-0">
+        <input
+          className="form-control mr-sm-2"
+          type="search"
+          placeholder="AuctionID"
+          aria-label="Search"
+          value={search}
+          onChange={e => handleInput(e)}
+        />
+        <Link className="auction-link" to={`/${search}`}>
           <button
             className="btn btn-outline-success my-2 my-sm-0"
             type="submit"
           >
             Search
           </button>
-        </form>
-      </nav>
-    );
-  }
-}
+        </Link>
+      </form>
+    </nav>
+  );
+};
 
 export default Navbar;
