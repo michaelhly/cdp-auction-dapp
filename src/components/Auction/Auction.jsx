52aedb69470f0e0a9b3d4230208f0faa8d912b3f
dontsellshort <michaelhly@gmail.com>
2019-01-10 09:28:24 -0600

    fixed bug

--- src/components/Auction/Auction.jsx
+++ src/components/Auction/Auction.jsx
@@ -180,10 +180,10 @@ const Auction = props => {
 
   const toggleButton = () => {
     if (web3.account !== auction.seller) return null;
+    if (auction.state > 1) return null;
     const active =
       auction.state === 0 &&
       auctionStatus(auction.state, auction.expiry) !== "Expired";
-
     return (
       <span>
         <button
